54c52c759d14a2be7f33c761a2f77388af1b65ef
adityardesai <adityardesai@gmail.com>
2016-11-05 23:53:40 -0700

    removing print lines

---
+++ babiLemma/Globals.py
@@ -0,0 +1,9 @@
+class GlobalsClass:
+    STANFORD_NER_PATH="/home/aditya/src/stanfordNER/stanford-ner-2015-12-09/stanford-ner.jar"
+    ENCODING="utf-8"
+    STANFORD_NER_CLASSIFIER="/home/aditya/src/stanfordNER/stanford-ner-2015-12-09/classifiers/english.all.3class.distsim.crf.ser.gz"
+    STANFORD_BABI_NER_CLASSIFIER="/home/aditya/src/stanfordNER/stanford-ner-2015-12-09/baby-model.ser.gz"
+    JSON_FILE="/home/aditya/newJavaSpace/babI/babiLemma/NER_JSON.txt"
+    NERTEXT_FILE="/home/aditya/newJavaSpace/babI/babiLemma/NER_TEXT.txt"
+    LEMMA_TEXT="/home/aditya/newJavaSpace/babI/Tests/Test_single/lemmaTest"
+    CORENLP_SERVER="http://localhost:9000"
--- babiLemma/babiLemma.py
+++ babiLemma/babiLemma.py
@@ -4,12 +4,14 @@ import os
 import collections
 import json
 from pycorenlp import StanfordCoreNLP
+from Globals import GlobalsClass
 
 class babiLemma(object):
-    def __init__(self, corenlp_url='http://localhost:9000'):
+    def __init__(self, corenlp_url=GlobalsClass.CORENLP_SERVER):
         self.corenlp = StanfordCoreNLP(corenlp_url)
+        self.writeString=""
         pass
-    def tokenizeInput(self, trainFilePath):
+    def readInput(self, trainFilePath):
         print("Files are beig read and tokenized ...")
         props = { 'annotators': 'tokenize,ssplit,pos,lemma', 'outputFormat': 'json'}
         for root, dirs, files in os.walk(trainFilePath):
@@ -17,33 +19,42 @@ class babiLemma(object):
                 filePath = os.path.join(root, presentFile)
                 with io.open(filePath, "r") as sFile:
                     for textLine in sFile: 
-                        #textLine=textLine.split(" ")
                         output = self.corenlp.annotate(textLine, properties=props)
-                        #print(output)
-                        self.parseOutput(textLine,output)
-    def parseOutput(self,textLine,output):
-        resLabel=""
-        originalText=""
-        lemma=""
-        result=dict()
-        result['Sentence']=textLine
+                        self.parseOutput(textLine, output)
+    def parseOutput(self, textLine, output):
+        resString = ""
+        originalText = ""
+        lemma = ""
+        resultDict = dict()
+        resultDict['Sentence'] = textLine
         for sentence in output['sentences']:
             for tok in sentence['tokens']:
-                originalText=tok['originalText']
-                if(tok['pos']=='VBD'):
-                    resLabel+="VBD"+"\t"+originalText+"\t"
-                    lemma+="\t"+tok['lemma']+"\t"
-                    result['VBD']=originalText
-                    result['Lemma']=tok['lemma']
-                elif(tok['pos']=='NNP'):
-                    resLabel+="NNP"+"\t"+originalText+"\t"
-                    result['NNP']=originalText
-                elif(tok['pos']=='NN'):
-                    resLabel+="NN"+"\t"+originalText+"\t"
-                    result['NN']=originalText
-        #print(textLine.strip("\n")+"\t"+"{"+resLabel+"}"+"\t"+"VBD Lemma"+"["+lemma+"]")
-        s3 = json.dumps(result, indent=4, sort_keys=True)
-        print(s3)
+                originalText = tok['originalText']
+                if(tok['pos'] == 'VBD' or tok['pos'] == 'VB' or tok['pos'] == 'VBG' or tok['pos'] == 'VBN'or tok['pos'] == 'VBP'or tok['pos'] == 'VBZ'):
+                    resString += "Verb" + "\t" + originalText + "\t"
+                    lemma += "\t" + tok['lemma'] + "\t"
+                    resultDict['Verb'] = originalText
+                    resultDict['Lemma'] = tok['lemma']
+                elif(tok['pos'] == 'NNP'):
+                    resString += "NNP" + "\t" + originalText + "\t"
+                    resultDict['NNP'] = originalText
+                elif(tok['pos'] == 'NN'):
+                    resString += "NN" + "\t" + originalText + "\t"
+                    resultDict['NN'] = originalText
+        self.writeString = self.writeString+textLine.strip("\n") + "\t" + "{" + resString + "}" + "\t" + "Verb Lemma" + "[" + lemma + "]"+"\n"
+        # print(writeString)   
+        #s3 = json.dumps(resultDict, indent=4, sort_keys=True)
+        # print(s3)
+        #self.writeToFile(writeString, "TRUE")
+    def writeToFile(self, str, json):
+        if(json == "TRUE"):
+            with open(GlobalsClass.JSON_FILE, "a+") as textFile:
+                textFile.write(str)
+        elif(json == "FALSE"):   
+             with open(GlobalsClass.NERTEXT_FILE, "a+") as textFile:
+                textFile.write(self.writeString) 
+
 if __name__ == "__main__":
     babiLemma = babiLemma()
-    babiLemma.tokenizeInput("/home/aditya/newJavaSpace/babI/Tests/Test_single/lemmaTest")
+    babiLemma.readInput(GlobalsClass.LEMMA_TEXT)
+    babiLemma.writeToFile("","FALSE")
