36fe09e4d00f2cf6ef7a40ea3cae298125ff29b6
adityardesai <adityardesai@gmail.com>
2016-11-12 18:30:14 -0800

    adding fact number to output

--- babiLemma/babiLemma.py
+++ babiLemma/babiLemma.py
@@ -1,5 +1,6 @@
 import sys
 import io
+import re
 import os
 import collections
 import json
@@ -22,9 +23,11 @@ class babiLemma(object):
                 filePath = os.path.join(root, presentFile)
                 with io.open(filePath, "r") as sFile:
                     for textLine in sFile: 
+                        factNum=re.findall(r'\d+',textLine)
+                        textLine=''.join([i for i in textLine if not i.isdigit()])
                         output = self.corenlp.annotate(textLine, properties=props)
-                        self.parseOutput(textLine, output)
-    def parseOutput(self, textLine, output):
+                        self.parseOutput(factNum,textLine, output)
+    def parseOutput(self,factNum,textLine, output):
         resString = ""
         originalText = ""
         lemma = ""
@@ -33,6 +36,7 @@ class babiLemma(object):
         for sentence in output['sentences']:
             for tok in sentence['tokens']:
                 originalText = tok['originalText']
+                resultDict['factNum'] = factNum
                 if(tok['pos'] == 'VBD' or tok['pos'] == 'VB' or tok['pos'] == 'VBG' or tok['pos'] == 'VBN'or tok['pos'] == 'VBP'or tok['pos'] == 'VBZ'):
                     resString += "\"POS_Verb\": [" + originalText+"],"
                     lemma +=tok['lemma']
@@ -44,23 +48,25 @@ class babiLemma(object):
                 elif(tok['pos'] == 'NN'):
                     resString += "\"POS_NN\": [" + originalText +"]"
                     resultDict['POS_NN'] = originalText
-        self.writeString = self.writeString+"{\"sentence\": "+"\""+textLine.strip("\n") +"\""+ "," + resString + "," + "\"Verb Lemma\"" + ": [" + lemma + "]}"+"\n"
+        #self.writeString = self.writeString+"{\"factNum\": "+str(self.count)+","+"\"sentence\": "+"\""+textLine.strip("\n") +"\""+ "," + resString + "," + "\"Verb Lemma\"" + ": [" + lemma + "]}"+"\n"
+        with open(GlobalsClass.NERTEXT_FILE, "a+") as textFile:
+            json.dump(resultDict,textFile)
+            textFile.write("\n")
         string1 = json.dumps(resultDict, indent=4, sort_keys=True)
-        self.tempDict[self.count] = resultDict
-        self.count+=1
-        
+        self.tempDict[factNum[0]] = resultDict
+                
     def writeToFile(self, str, json):
         if(json == "TRUE"):
             with open(GlobalsClass.JSON_FILE, "a+") as textFile:
                 textFile.write(str)
         elif(json == "FALSE"):   
-             with open(GlobalsClass.NERTEXT_FILE, "a+") as textFile:
-                textFile.write(self.writeString) 
+            with open(GlobalsClass.NERTEXT_FILE, "a+") as textFile:
+               textFile.write(self.writeString) 
 
 if __name__ == "__main__":
     babiLemma = babiLemma()
     babiLemma.readInput(GlobalsClass.LEMMA_TEXT)
-    babiLemma.writeToFile("","FALSE")
     jsonString = json.dumps(babiLemma.tempDict, indent=4, sort_keys=True)
     babiLemma.writeToFile(jsonString, "TRUE")
-    #print(babiLemma.writeString)
+    print("Done")
+    
